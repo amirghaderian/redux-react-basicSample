f5436ea320d7eadd00327a5ecef0d05ba970e951
asd <asd.it@gmail.com>
2023-11-20 13:21:50 +0330

    Add reducer to redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
-import './App.css'
+import { Provider } from "react-redux";
+import "./App.css";
+import Cake from "./components/Cake";
+import store from "./redux/store";
 
 const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <Provider store={store}>
+      <Cake/>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
+
+// store : cake ,=> 1.store , 2. action 3.reducer
---
+++ src/components/Cake.jsx
@@ -0,0 +1,8 @@
+const Cake = () => {
+  return (
+    <div><h2>Cake Number:10 </h2> 
+    <button>Buy Cake</button> </div>
+  )
+}
+
+export default Cake
